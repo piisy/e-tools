ea9dd94652d38471d690cd1acdf1f3e067bee7b7
powerfulyang <i@powerfulyang.com>
2024-10-12 18:44:26 +0800

    feat: demo

---
+++ rsbuild.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@rsbuild/core'
+import { pluginBabel } from '@rsbuild/plugin-babel'
+import { pluginSolid } from '@rsbuild/plugin-solid'
+
+export default defineConfig({
+  plugins: [
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+    }),
+    pluginSolid(),
+  ],
+  source: {
+    entry: {
+      index: './src/demo/index.tsx',
+    },
+  },
+})
