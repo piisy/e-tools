12da5d2d7e01afeb9582df328aa67d4ddbc08b25
eleven <hutyxxx@gmail.com>
2025-01-03 18:18:09 +0800

    feat: atLeast

--- src/promise/wait.ts
+++ src/promise/wait.ts
@@ -1,3 +1,15 @@
 export function waitFor(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
+
+export async function atLeast(
+  promise: Promise<unknown>,
+  ms: number,
+) {
+  const result = await Promise.allSettled([
+    waitFor(ms),
+    promise,
+  ])
+
+  return result[1]
+}
